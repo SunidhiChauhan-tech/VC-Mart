e3909c416a4132ce1deab29cc02e9f8f79be996c
SunidhiChauhan-tech <sunidhichauhan262005@gmail.com>
2026-09-13 15:31:58 +0530

    Fix production CORS

--- server/server.js
+++ server/server.js
@@ -36,7 +36,8 @@ app.use("/api", apiLimiter);
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-];
+  process.env.FRONTEND_URL,
+].filter(Boolean);
 
 app.use(
   cors({
